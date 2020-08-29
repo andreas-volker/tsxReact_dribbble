9701fe5ccc71e474e669b371510921a7f9d26d00
Andreas Karel Völker <andreaskvolker@gmail.com>
2020-08-29 18:47:48 -0300

    install @types

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'reset.css';
+import Roteador from 'Roteador';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Roteador />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
